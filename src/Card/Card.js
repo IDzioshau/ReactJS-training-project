097d851f68cfc2529e5714f38c2ce703c9eb8fdd
Ihar Dzioshau <IDzioshau@gomel.iba.by>
2021-01-03 16:30:33 +0300

    ReactJS - task2 - task 2 is done

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 const Card = props => {
+    const [cardState, setCardState] = useState({
+        style: "card"
+    });
+
+    const switchStyle = event => {
+        let newStyle = event.target.checked ? "card2" : "card";
+        setCardState({
+            style: newStyle
+        });
+    };
+
     return (
-        <div className="card">
+        <div className={cardState.style}>
+            <input className="style-checkbox" type="checkbox" onChange={switchStyle} />
             <h1>{props.caption}</h1>
             <hr />
             <p>{props.text}</p>
